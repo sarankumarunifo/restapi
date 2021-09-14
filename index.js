da1207b25ab360a029097558f3e1ec5e1c2baaba
sarankumarunifo <89625600+sarankumarunifo@users.noreply.github.com>
2021-09-14 19:28:09 +0530

    final push

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const employeeDetails=require("./data.json")
+const employeeDetails = require("./data.json")
 
 const Hapi = require('@hapi/hapi');
 
@@ -10,50 +10,87 @@ const init = async () => {
         host: 'localhost'
     });
 
+    // get all
     server.route({
         method: 'GET',
         path: '/getEmployeeDetails',
-        handler: () => {
+        handler: (request) => {
+            console.log(request.payload)
             return employeeDetails;
         }
     });
-
     server.route({
-        method:'POST',
-        path:'/addEmployee',
+        method: 'GET',
+        path: '/getEmployeeDetailsId/{id?}',
+        handler: (request) => {
+            const getpayload = request.payload;
+            const index = employeeDetails.employees.findIndex(emp => emp.id === request.query.id);
+            console.log(index)
+            return employeeDetails.employees[index]
+
+        }
+    });
+
+    
+let flag=1;
+let index=[]
+    server.route({
+        method: 'POST',
+
+        path: '/addEmployee',
         handler: (request) => {
             const payload = request.payload;
-            employeeDetails.employees.push(payload);
+             index = employeeDetails.employees?.map(emp => (emp.id) )
+             console.log(index)
+            console.log(payload.id +","+ index)
+        //    console.log(employeeDetails.employees.length)
+            for(let i=1; i<index.length;i++)
+            {
+                if( payload.id === index[i]){
+                    flag = 0
+                }
+            }
+            console.log("flag"+flag)
+            if(flag != 1)
+            {
+                return ` id is already taken `
+            }
+            else{
+                employeeDetails.employees.push(payload);
+            }
             console.log(employeeDetails);
+
             return `New Employee added!: ${payload.firstName} and ${payload.lastName}`;
         }
     })
 
     server.route({
-        method:'PUT',
-        path:'/updateEmployee/{firstName?}',
-        handler:(request)=>{
+        method: 'PUT',
+        path: '/updateEmployee/{id?}',
+        handler: (request) => {
             const updatePayload = request.payload;
-            const index=employeeDetails.employees.findIndex(emp=>emp.firstName===request.params.firstName);
-            if(index===-1) {
-                return `Employee does not exists! ${request.params.firstName}`;
+            const index = employeeDetails.employees.findIndex(emp => emp.id === request.query.id);
+            console.log(index)
+            if (index === -1) {
+                return `Employee does not exists! ${request.query.id}`;
             }
-            employeeDetails.employees[index].firstName=updatePayload.firstName;
-            employeeDetails.employees[index].lastName=updatePayload.lastName;
-            return `Employee updated!`;
+            employeeDetails.employees[index].firstName = updatePayload.firstName;
+            employeeDetails.employees[index].lastName = updatePayload.lastName;
+            return `Employee updated!: ${updatePayload.firstName}`;
         }
     })
 
     server.route({
         method: 'DELETE',
-        path: '/deleteEmployee',
+        path: '/deleteEmployee/{id?}',
         handler: (request) => {
             const deletePayload = request.payload;
-            const index=employeeDetails.employees.findIndex(emp=>emp.firstName===deletePayload.firstName);
-            if(index===-1) {
+            const index = employeeDetails.employees.findIndex(emp => emp.id === request.query.id);
+            console.log(index)
+            if (index === -1) {
                 return `Given employee firstName does not exists: ${request.params.firstName}`;
             }
-            employeeDetails.employees.splice(index,1);
+            employeeDetails.employees.splice(index, 1);
             return 'Employee deleted!!!';
         }
     })
